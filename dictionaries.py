f61827e2ed8183f6d3b7b1d6079c59f7b5d79af1
Erik Hernandez <erikdhv@gmail.com>
2020-05-15 14:51:33 -0500

    Uso de diccionarios con python

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+"""[Aquí realizaremos una muestra básica de uso de diccionarios]
+    Tuplas se usan con ()
+    listas con []
+    diccionarios con {}
+"""
+
+diccionario = {'Erik':37, 'Dario':20, 'Stric':25, 'Maria':37}
+
+print(diccionario['Dario'])
+
+print(len(diccionario))
+
+print(diccionario)
